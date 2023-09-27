931d04f27cf5266c48c7ee560df5ed37d4c5b8b3
DEV200019 <error@DESKTOP-8PN7PJI>
2023-09-26 17:22:10 -0700

    Product details

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //this is to get the id from html
-const playerListEl = document.querySelector('#product-list');
+const productListEl = document.querySelector('#product-list');
+const productDetailsEL = document.querySelector('#product-details')
+
 //fetching product in console
 const fetchProduct = async ()=> {
    // fetch('https://fakestoreapi.com/products')
@@ -18,7 +20,7 @@ const fetchProduct = async ()=> {
 
 //going to render the products
 const renderProducts = products => {
-    const template = products.map(({id,image,title,description,price,category})=> (
+    const template = products.map(({id,image,title,description,price,category},index)=> (
         `<li class= "product">
                  <img src="${image}">
                  <h3>${title}</h3>
@@ -28,9 +30,9 @@ const renderProducts = products => {
                  <button data-id="${id}">See Details</button>
                 </li>`
     ))
-    playerListEl.innerHTML = template.join('');
+    productListEl.innerHTML = template.join('');
 }
-playerListEl.addEventListener('click', async e =>{
+productListEl.addEventListener('click', async e =>{
     if(e.target.matches('button')){
         const id= e.target.dataset.id;
         const res= await fetch(`https://fakestoreapi.com/products/${id}`);
@@ -38,8 +40,31 @@ playerListEl.addEventListener('click', async e =>{
         console.log(product)
 
     }
+    })
+
+    const renderProductDetails = product =>{
+        console.log("Making it here")
+        const template = `
+        <div class="product-details">
+        <img src="${product.image}" alt = "img">
+        <h3> ${product.title}</h3>
+        <p>${product.description}</p>
+        <p>Price:$${product.price}</p>
+        <p>${product.category}</p>
+        </div>
+        `
+        productDetailsEL.innerHTML = template;
+    }
     
-})
+
+    productListEl.addEventListener('click', async e =>{
+        if(e.target.matches('button')){
+            const id = e.target.dataset.id;
+            const res = await fetch (`https://fakestoreapi.com/products/${id}`)
+            const product =await res.json();
+            renderProductDetails(product);
+        }
+    })
 const init = async () => {
     const products = await fetchProduct();
     renderProducts(products);}
